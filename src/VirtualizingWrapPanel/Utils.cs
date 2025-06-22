af2c012c1eeb38a9a84f97d8e86008b7bae8f276
sbaeumlisberger <s.baeumlisberger@live.de>
2025-06-22 22:59:35 +0200

    Fix exception if endIndex is out of bounds and add some doc

--- src/VirtualizingWrapPanel/Utils.cs
+++ src/VirtualizingWrapPanel/Utils.cs
@@ -4,13 +4,19 @@ namespace WpfToolkit.Controls;
 
 internal class Utils
 {
-
+    /// <summary>
+    /// Creates a HashSet from a range of elements in a collection. 
+    /// Start and end index are inclusive and can be out of bounds. 
+    /// </summary>
+    /// <param name="startIndex">Index of the first item to take (inclusive).</param>
+    /// <param name="endIndex">Index of the last item to take (inclusive).</param>
     public static HashSet<T> HashSetOfRange<T>(IList<T> collection, int startIndex, int endIndex)
     {
         var hashSet = new HashSet<T>();
         if (startIndex >= 0)
         {
-            for (int i = startIndex; i <= endIndex; i++)
+            int count = collection.Count;
+            for (int i = startIndex; i <= endIndex && i < count; i++)
             {
                 hashSet.Add(collection[i]);
             }
