9de940ed220e9a984a45c22503c8b178365559bd
sbaeumlisberger <s.baeumlisberger@live.de>
2025-07-27 19:52:50 +0200

    #87 Add test

--- src/VirtualizingWrapPanelTest/Tests/RegressionTest.cs
+++ src/VirtualizingWrapPanelTest/Tests/RegressionTest.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows.Data;
+﻿using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
 using WpfToolkit.Controls;
 using Xunit;
 
@@ -7,7 +10,7 @@ namespace VirtualizingWrapPanelTest.Tests;
 public class RegressionTest
 {
 
-    [UIFact]
+    [WpfFact]
     public void RemoveVirtualizingWrapPanelFromItemsControlAndRemoveItem()
     {
         var items = TestUtil.GenerateItems(1000);
@@ -24,5 +27,21 @@ public class RegressionTest
         // If no exception is thrown, the test passes.
     }
 
+    // #87 DivideByZeroException when item width greater than viewport width and cache unit is Item
+    [WpfTheory]
+    [InlineData(VirtualizationCacheLengthUnit.Item, 10)]
+    [InlineData(VirtualizationCacheLengthUnit.Page, 1)]
+    [InlineData(VirtualizationCacheLengthUnit.Pixel, 100)]
+    public void ItemWidthGreaterThanViewportWidth(VirtualizationCacheLengthUnit cacheLengthUnit, int cacheLength)
+    {
+        var vwp = TestUtil.CreateVirtualizingWrapPanel(500, 400);
+        VirtualizingPanel.SetCacheLengthUnit(vwp.ItemsControl, cacheLengthUnit);
+        VirtualizingPanel.SetCacheLength(vwp.ItemsControl, new VirtualizationCacheLength(cacheLength));
+        vwp.SetVerticalOffset(1000);
+        vwp.UpdateLayout();
+
+        vwp.ItemSize = new Size(vwp.ItemsControl.Width + 100, 100);
+        vwp.UpdateLayout();
+    }
 
 }
